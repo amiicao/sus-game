3b07fc07b354fdc39aa8397b4a1457c065dc1d64
Amy Cao <Amy.369@hotmail.com>
2021-09-05 01:05:08 +0800

    all images and sprites are here

--- main.py
+++ main.py
@@ -6,33 +6,41 @@ import os
 pygame.init()
 
 FPS = 60
-
-#images
-#BACKGROUND = pygame.image.load(os.path.join('assets','BACKGROUND.png'))
-#BLUE_BIN = pygame.image.load(os.path.join('assets','BLUE_BIN.png'))
-#GREEN_BIN = pygame.image.load(os.path.join('assets','GREEN_BIN.png'))
-#BLACK_BIN = pygame.image.load(os.path.join('assets','BLACK_BIN.png'))
+BIN_WIDTH, BIN_HEIGHT = 300, 250
+WIDTH, HEIGHT = 800, 800
 
 # Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+#images
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('assets','BACKGROUND.jpg')),(WIDTH,HEIGHT))
+BLUE_BIN_IMAGE = pygame.image.load(os.path.join('assets','BLUE_BIN.png'))
+BLUE_BIN = pygame.transform.scale(BLUE_BIN_IMAGE, (BIN_WIDTH, BIN_HEIGHT))
+GREEN_BIN_IMAGE = pygame.image.load(os.path.join('assets','GREEN_BIN.png'))
+GREEN_BIN = pygame.transform.scale(GREEN_BIN_IMAGE, (BIN_WIDTH, BIN_HEIGHT))
+BLACK_BIN_IMAGE = pygame.image.load(os.path.join('assets','BLACK_BIN.png'))
+BLACK_BIN = pygame.transform.scale(BLACK_BIN_IMAGE, (BIN_WIDTH, BIN_HEIGHT))
+YELLOW_BIN_IMAGE = pygame.image.load(os.path.join('assets','YELLOW_BIN.png'))
+YELLOW_BIN = pygame.transform.scale(YELLOW_BIN_IMAGE, (BIN_WIDTH, BIN_HEIGHT))
 
 def draw_window():
-    # Add the Bins on lower screen
-    #screen.blit(BLUE_BIN, (400, 183 ))
-    #screen.blit(GREEN_BIN, (400, 316))
-    #screen.blit(BLACK_BIN, (400, 450))
+    
+    screen.blit(BACKGROUND, (0,0))
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
+    # Add the Bins on NESW of screen
+    screen.blit(BLUE_BIN, (450, 250 ))
+    screen.blit(GREEN_BIN, (50, 250))
+    screen.blit(BLACK_BIN, (250, 450))
+    screen.blit(YELLOW_BIN, (250, 50))
 
     # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    pygame.draw.circle(screen, (245, 245, 220), (400, 400), 75)
 
     # Flip the display
-    pygame.display.flip()
+    #pygame.display.flip()
 
     # Update the screen
-    #pygame.display.update()
+    pygame.display.update()
 
 
 def main():
@@ -41,12 +49,11 @@ def main():
     running = True
     while running:
         clock.tick(FPS)
-
+        draw_window()
         # Did the user click the window close button?
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        draw_window()
     # Done! Time to quit.
     pygame.quit()
 if __name__ == "__main__":
