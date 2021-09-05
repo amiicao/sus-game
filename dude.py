413f16d0c4717ff05aa1b57026ac6c686dd89e00
brianfu <fubof@sfu.ca>
2021-09-05 10:32:01 -0700

    tiny fix; can't whack pieces into himself

--- dude.py
+++ dude.py
@@ -84,10 +84,11 @@ class Dude(Unit):
         for piece in t_list:
             #if(self.collide_rect.colliderect(piece.collide_rect) and self.stance == Stance.REST and temp == True):
             if(self.collide_rect.colliderect(piece.collide_rect) and self.stance == Stance.REST ):
-                piece.hit_guy()
-                garbage_list.remove(piece); del piece
-                self.injured()
-                print ("been hit")
+                if(piece.head_inwards): #Don't let the guy whack pieces into himself
+                    piece.hit_guy()
+                    garbage_list.remove(piece); del piece
+                    self.injured()
+                    print ("been hit")
 
     #passing health to
     def getHealth(self):
